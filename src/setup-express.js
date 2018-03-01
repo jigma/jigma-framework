48fdc3949f5878c083827302ae8b9cb8677f9050
simonsmadsen <simon@smadsen.com>
2018-03-01 09:53:12 +0100

    cors x-header fix

--- src/setup-express.js
+++ src/setup-express.js
@@ -4,10 +4,13 @@ const helmet = require('helmet')
 const app = express()
 const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Headers', 'x-access-token,Content-Type,Accept')
+  res.setHeader('Access-Control-Allow-Methods', 'POST')
   next()
 }
 app.use(allowCrossDomain)
 app.use(bodyParser.json())
 app.use(helmet())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 export default app
